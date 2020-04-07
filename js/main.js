5fe7ca791b056ca990951427db5be626481c89fe
Louielo <56979318+Louielo@users.noreply.github.com>
2020-04-08 01:01:13 +0200

    réserver

--- js/main.js
+++ js/main.js
@@ -20,6 +20,13 @@ MAIN JS
 //      $('#buttonbat').hide();
 // });
 
+//RESERVER
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function () {
+	$(this).parent(".sub-menu").children("ul").slideToggle("100");
+	$(this).find(".right");
+});
+
 
 
 (function($) {
